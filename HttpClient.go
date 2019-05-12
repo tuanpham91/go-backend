98375a69c877728c090c990b44f01976a9011260
Anh Tuan Pham <anh.pham@jambit.com>
2019-05-12 11:58:32 +0200

    Basic Client

---
+++ HttpClient.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+// simple  
+
+func main() {
+
+}
+
+func sendRequestToEndpoint(endPoint String) {
+	
+}
+
---
+++ RequestType.go
@@ -0,0 +1,4 @@
+type RequestType String 
+const (
+	
+)
